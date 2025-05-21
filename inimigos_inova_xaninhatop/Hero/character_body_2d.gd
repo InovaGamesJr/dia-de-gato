b51b490b407f63150bc826701d8f3f30c5392b61
ViniciusMudo <paulo.mudo2016@outlook.com>
2025-05-21 18:45:22 -0300

    Adcionar o inicio do codigo do gato

--- inimigos_inova_xaninhatop/Hero/character_body_2d.gd
+++ inimigos_inova_xaninhatop/Hero/character_body_2d.gd
@@ -1,39 +1,74 @@
 extends CharacterBody2D
 
+#Exports para mexer diretamentes nos nós
+@onready var spriteGato: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -350.0
-const Gravity = 1000
-const max_jumps = 2
-var number_jumps = 0
+#Estados do player
+enum states {
+	idle, 
+	running, 
+	jumping, 
+	dash, 
+	climbing, 
+	knockback
+	}
+var current_state : states
+
+#Variaveis da fisica
+var speed : int = 140
+var jumpingForce : int = -250
+var forceGravity : int = 800
+var knockbackDir = Vector2()
+
+#Booleanos
+var doubleJump : bool = false
+var dashBool : bool = true
 
 func _ready():
 	Global.playerBody = self
+	current_state = states.idle
 
-func _physics_process(delta: float):
-	# Add the gravity.d
-	if not is_on_floor():
-		velocity.y += Gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		number_jumps += 1
-	elif Input.is_action_just_pressed("Jump") and !is_on_floor() and number_jumps == 1:
-		velocity.y = JUMP_VELOCITY
-		number_jumps += 1
-	elif Input.is_action_just_pressed("Jump") and !is_on_floor() and number_jumps == 0:
-		velocity.y = JUMP_VELOCITY
-		number_jumps += 2
-	# Restart jump_count
-	if is_on_floor():
-		number_jumps = 0
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("Left", "Right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func _physics_process(delta):
+	gravity(delta)
+	player_actions()
+	match current_state:
+		states.idle:
+			idle()
+		states.running:
+			running(delta)
+		states.jumping:
+			pass
+		states.dash:
+			pass
+		states.climbing:
+			pass
+		states.knockback:
+			pass
+	#print("Estado: ", states.keys()[current_state])
 
 	move_and_slide()
+
+func gravity(delta : float):
+	if !is_on_floor():
+		velocity.y = forceGravity * delta 
+
+func player_actions():
+	if Input.get_axis("Left", "Right"):
+		current_state = states.running
+
+func idle():
+	if is_on_floor():
+		spriteGato.play("Idle")
+
+func running(delta : float):
+	var direction = Input.get_axis("Left", "Right") # 1 ou -1
+	if direction:
+		velocity.x = lerp(velocity.x, direction * speed, 1)
+		if is_on_floor():
+			spriteGato.play("Running")
+	else:
+		velocity.x = 0
+		current_state = states.idle
+	
+	if direction != 0:
+		spriteGato.flip_h = false if direction > 0 else true #Operador Tenario, usar if em uma linha, se a direcao for 1 (direita) o flip_h vai ser FALSE(mantendo a orientacao original), caso contrario -1(Esquerda) o flip_h se torna TRUE mudando a direcao do personagem pra a esquerda.
